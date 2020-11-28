20f7eb9992d6df57df03712e5ea3a3b6aa4a0f18
Andrew <andrew.casin@gmail.com>
2020-11-28 03:04:59 -0600

    commit tree 38c4e2cf78a797391e8f013d6cc23ff1998051fd

--- lib/student.rb
+++ lib/student.rb
@@ -4,20 +4,30 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(student_hash) #create new student out of hash
+    student_hash.each do |attribute, value| #for each hash pair, send key value pairs to attribute= methods to initialize with those values
+      self.send("#{attribute}=", value)
+    end
+    @@all << self 
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students_array) #uses the Scraper class to create new students with the correct name and location.
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  #uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student.
+  def add_student_attributes(attributes_hash) 
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self    
   end
 
-  def self.all
-    
+  def self.all #return class variable @@all
+    @@all    
   end
+
 end
 
